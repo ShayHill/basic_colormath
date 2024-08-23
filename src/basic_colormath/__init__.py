256388873d20d95203820cff6009fddb50dcd042
Shay Hill <shay_public@hotmail.com>
2024-08-23 10:43:11 -0500

    feat: add vectorized functions to project namespace

--- src/basic_colormath/__init__.py
+++ src/basic_colormath/__init__.py
@@ -4,6 +4,14 @@
 :created: 2023-04-30
 """
 
+# Numpy is an optional dependency. If it's not installed, the numpy functions will
+# not be available.
+try:
+    import numpy
+except ImportError:
+    numpy = None
+
+
 from basic_colormath.conversion import (
     float_to_8bit_int,
     float_tuple_to_8bit_int_tuple,
@@ -48,3 +56,42 @@ __all__ = [
     "scale_hex",
     "scale_rgb",
 ]
+
+if numpy:
+    from basic_colormath.vec_conversion import (
+        floats_to_uint8,
+        hexs_to_rgb,
+        hsls_to_rgb,
+        hsvs_to_rgb,
+        rgbs_to_hex,
+        rgbs_to_hsl,
+        rgbs_to_hsv,
+    )
+    from basic_colormath.vec_distance import (
+        get_deltas_e,
+        get_deltas_e_hex,
+        get_deltas_e_lab,
+        get_euclideans,
+        get_euclideans_hex,
+        get_sqeuclideans,
+        get_sqeuclideans_hex,
+        rgbs_to_lab,
+    )
+
+    __all__ += [
+        "floats_to_uint8",
+        "get_deltas_e",
+        "get_deltas_e_hex",
+        "get_deltas_e_lab",
+        "get_euclideans",
+        "get_euclideans_hex",
+        "get_sqeuclideans",
+        "get_sqeuclideans_hex",
+        "hexs_to_rgb",
+        "hsls_to_rgb",
+        "hsvs_to_rgb",
+        "rgbs_to_hex",
+        "rgbs_to_hsl",
+        "rgbs_to_hsv",
+        "rgbs_to_lab",
+    ]
